47ba6e505d62182728dc7a6186596f9d9c940d17
sol <mssarratea@gmail.com>
2020-09-05 19:57:03 -0400

    init config

---
+++ README.md
@@ -0,0 +1,18 @@
+## my sexy emacs 
+
+Repository for loving my working environment: 
+
+- started from http://tuhdo.github.io/c-ide.html
+- customize gdb 
+
+
+![](https://media0.giphy.com/media/sIIhZliB2McAo/200.gif)
+
+
+Resources for learning emacs: 
+ - [org-mode](https://sachachua.com/blog/2014/01/tips-learning-org-mode-emacs/)
+ - [nice visual beginner's guide](https://sachachua.com/blog/wp-content/uploads/2013/05/How-to-Learn-Emacs-v2-Large.png)
+ - [emacs is sexy](https://emacs.sexy/)
+ 
+ 
+  ![](https://imgs.xkcd.com/comics/lisp.jpg)
---
+++ elpa/yasnippet-snippets-20200802.1658/yasnippet-snippets-autoloads.el
@@ -0,0 +1,29 @@
+;;; yasnippet-snippets-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "yasnippet-snippets" "yasnippet-snippets.el"
+;;;;;;  (24403 46308 99524 231000))
+;;; Generated autoloads from yasnippet-snippets.el
+
+(autoload 'yasnippet-snippets-initialize "yasnippet-snippets" "\
+Load the `yasnippet-snippets' snippets directory.
+
+\(fn)" nil nil)
+
+(eval-after-load 'yasnippet '(yasnippet-snippets-initialize))
+
+;;;***
+
+;;;### (autoloads nil nil ("yasnippet-snippets-pkg.el") (24403 46407
+;;;;;;  967517 874000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; yasnippet-snippets-autoloads.el ends here
